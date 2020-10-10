5008cc7b224d5f1e7fce7f444dc87989a7e6c0ac
zhangyi-he <jefferyhe@outlook.com>
2020-10-10 20:25:10 +0100

    Update CFUN_COL.cpp

--- v1.2/CFUN_COL.cpp
+++ v1.2/CFUN_COL.cpp
@@ -2,7 +2,6 @@
 // Xiaoyang Dai, Sile Hu, Mark Beaumont, Feng Yu, Zhangyi He
 
 // version 1.2
-
 // Horse coat colours (ASIP & MC1R) under non-constant natural selection and non-constant demographic histories (N/A is not allowed)
 
 // C functions
