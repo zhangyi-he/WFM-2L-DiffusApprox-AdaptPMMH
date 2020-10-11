01d6a128e5010847e8d50e6994c8a07ee7901779
zhangyi-he <jefferyhe@outlook.com>
2020-10-11 12:28:29 +0100

    Update TEST_COL.R

--- v1.2/TEST_COL.R
+++ v1.2/TEST_COL.R
@@ -299,7 +299,7 @@ points(smp_gen, smp_gen_frq[9, ], col = 'red', pch = 17, cex = 1)
 ################################################################################
 
 #' Generate a simulated dataset under the Wright-Fisher model
-test_seed <- 27
+test_seed <- 21
 set.seed(test_seed)
 
 model <- "WFM"
@@ -607,7 +607,7 @@ save(sel_cof, rec_rat, pop_siz, ref_siz, evt_gen, smp_gen, smp_siz, smp_cnt, ptn
 
 load("./Output/Output v1.2/TEST_PMMH.rda")
 
-pdf(file = "./Output/Output v1.2/TEST_PMMH_Traceplot.pdf", width = 12, height = 9)
+pdf(file = "./Output/Output v1.2/TEST_PMMH_Traceplot_SelCoeff.pdf", width = 12, height = 9)
 par(mfrow = c(4, 1), mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
 plot(1:itn_num, sel_cof_chn[1, 1, 1:itn_num], type = 'l',
      xlab = "Iteration", ylab = "Selection coefficient",
@@ -643,7 +643,7 @@ sel_cof_hpd[2, , 1] <- HPDinterval(as.mcmc(sel_cof_chn[2, 1, ]), prob = 0.95)
 sel_cof_hpd[1, , 2] <- HPDinterval(as.mcmc(sel_cof_chn[1, 2, ]), prob = 0.95)
 sel_cof_hpd[2, , 2] <- HPDinterval(as.mcmc(sel_cof_chn[2, 2, ]), prob = 0.95)
 
-pdf(file = "./Output/Output v1.2/TEST_PMMH_Posterior.pdf", width = 12, height = 9)
+pdf(file = "./Output/Output v1.2/TEST_PMMH_Posterior_SelCoeff.pdf", width = 12, height = 9)
 par(mfrow = c(2, 2), mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
 hist(sel_cof_chn[1, 1, ], breaks = seq(min(sel_cof_chn[1, 1, ]), max(sel_cof_chn[1, 1, ]), length.out = 50), freq = FALSE,
      xlab = "Selection coefficient",
@@ -680,6 +680,35 @@ abline(v = sel_cof[2, 2], col = 'red', lty = 2, lwd = 2)
 abline(v = sel_cof_est[2, 2], col = 'black', lty = 2, lwd = 2)
 abline(v = sel_cof_hpd[2, 1, 2], col = 'blue', lty = 2, lwd = 2)
 abline(v = sel_cof_hpd[2, 2, 2], col = 'blue', lty = 2, lwd = 2)
+dev.off()
+
+dif_sel_chn <- sel_cof_chn[, 2, ] - sel_cof_chn[, 1, ]
+
+dif_sel_est <- rowMeans(dif_sel_chn)
+
+dif_sel_hpd <- matrix(NA, nrow = 2, ncol = 2)
+dif_sel_hpd[1, ] <- HPDinterval(as.mcmc(dif_sel_chn[1, ]), prob = 0.95)
+dif_sel_hpd[2, ] <- HPDinterval(as.mcmc(dif_sel_chn[2, ]), prob = 0.95)
+
+pdf(file = "./Output/Output v1.2/TEST_PMMH_Posterior_SelChange.pdf", width = 12, height = 9)
+par(mfrow = c(1, 2), mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
+hist(dif_sel_chn[1, ], breaks = seq(min(dif_sel_chn[1, ]), max(dif_sel_chn[1, ]), length.out = 50), freq = FALSE,
+     xlab = "Selection coefficient",
+     main = "Posterior for the change in black")
+lines(density(dif_sel_chn[1, ]), lwd = 2, col = 'black')
+abline(v = 0, col = 'red', lty = 2, lwd = 2)
+abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[1, 1], col = 'blue', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[1, 2], col = 'blue', lty = 2, lwd = 2)
+
+hist(dif_sel_chn[2, ], breaks = seq(min(dif_sel_chn[2, ]), max(dif_sel_chn[2, ]), length.out = 50), freq = FALSE,
+     xlab = "Selection coefficient",
+     main = "Posterior for the change in chestnut")
+lines(density(dif_sel_chn[2, ]), lwd = 2, col = 'black')
+abline(v = 0, col = 'red', lty = 2, lwd = 2)
+abline(v = dif_sel_est[2], col = 'black', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[2, 1], col = 'blue', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[2, 2], col = 'blue', lty = 2, lwd = 2)
 dev.off()
 
 ########################################
@@ -733,7 +762,7 @@ sel_cof_est <- BayesianProcedure$sel_cof_est
 
 sel_cof_hpd <- BayesianProcedure$sel_cof_hpd
 
-pdf(file = "./Output/Output v1.2/TEST_BayesianProcedure_Posterior.pdf", width = 12, height = 9)
+pdf(file = "./Output/Output v1.2/TEST_BayesianProcedure_Posterior_SelCoeff.pdf", width = 12, height = 9)
 par(mfrow = c(2, 2), mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
 hist(sel_cof_chn[1, 1, ], breaks = seq(min(sel_cof_chn[1, 1, ]), max(sel_cof_chn[1, 1, ]), length.out = 50), freq = FALSE,
      xlab = "Selection coefficient",
@@ -770,6 +799,33 @@ abline(v = sel_cof[2, 2], col = 'red', lty = 2, lwd = 2)
 abline(v = sel_cof_est[2, 2], col = 'black', lty = 2, lwd = 2)
 abline(v = sel_cof_hpd[2, 1, 2], col = 'blue', lty = 2, lwd = 2)
 abline(v = sel_cof_hpd[2, 2, 2], col = 'blue', lty = 2, lwd = 2)
+dev.off()
+
+dif_sel_chn <- BayesianProcedure$dif_sel_chn
+
+dif_sel_est <- BayesianProcedure$dif_sel_est
+
+dif_sel_hpd <- BayesianProcedure$dif_sel_hpd
+
+pdf(file = "./Output/Output v1.2/TEST_BayesianProcedure_Posterior_SelChange.pdf", width = 12, height = 9)
+par(mfrow = c(1, 2), mar = c(5.5, 5, 5.5, 2.5), cex.main = 2, cex.sub = 1.75, cex.axis = 1.75, cex.lab = 1.75)
+hist(dif_sel_chn[1, ], breaks = seq(min(dif_sel_chn[1, ]), max(dif_sel_chn[1, ]), length.out = 50), freq = FALSE,
+     xlab = "Selection coefficient",
+     main = "Posterior for the change in black")
+lines(density(dif_sel_chn[1, ]), lwd = 2, col = 'black')
+abline(v = 0, col = 'red', lty = 2, lwd = 2)
+abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[1, 1], col = 'blue', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[1, 2], col = 'blue', lty = 2, lwd = 2)
+
+hist(dif_sel_chn[2, ], breaks = seq(min(dif_sel_chn[2, ]), max(dif_sel_chn[2, ]), length.out = 50), freq = FALSE,
+     xlab = "Selection coefficient",
+     main = "Posterior for the change in chestnut")
+lines(density(dif_sel_chn[2, ]), lwd = 2, col = 'black')
+abline(v = 0, col = 'red', lty = 2, lwd = 2)
+abline(v = dif_sel_est[2], col = 'black', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[2, 1], col = 'blue', lty = 2, lwd = 2)
+abline(v = dif_sel_hpd[2, 2], col = 'blue', lty = 2, lwd = 2)
 dev.off()
 
 ################################################################################
