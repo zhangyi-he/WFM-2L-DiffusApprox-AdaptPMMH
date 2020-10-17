4ca5ff3828a5ebd60f3d0baf39f2c6754e1eec9f
zhangyi-he <jefferyhe@outlook.com>
2020-10-17 08:04:52 +0100

    Update TEST_COL.R

--- v1.2/TEST_COL.R
+++ v1.2/TEST_COL.R
@@ -19,6 +19,9 @@ library("ggplot2")
 #install.packages("plot3D")
 library("plot3D")
 
+#install.packages("emdbook")
+library("emdbook")
+
 # call R functions
 source("./Code/Code v1.0/Code v1.2/RFUN_COL.R")
 
@@ -37,7 +40,7 @@ source("./Code/Code v1.0/Code v1.2/RFUN_COL.R")
 # sel_cof <- matrix(c(0e+00, 5e-03, 1e-02, 5e-03), nrow = 2, ncol = 2)
 # rec_rat <- 5e-01
 # pop_siz <- c(rep(1e+04, length.out = 201), rep(5e+03, length.out = 200), rep(1e+04, length.out = 100))
-# int_frq <- c(6e-01, 2e-01, 1e-01, 1e-01)
+# int_frq <- c(5e-01, 2e-01, 2e-01, 1e-01)
 # evt_gen <- 210
 # int_gen <- 0
 # lst_gen <- 500
@@ -77,7 +80,7 @@ source("./Code/Code v1.0/Code v1.2/RFUN_COL.R")
 # rec_rat <- 5e-01
 # pop_siz <- c(rep(1e+04, length.out = 201), rep(5e+03, length.out = 200), rep(1e+04, length.out = 100))
 # ref_siz <- 1e+04
-# int_frq <- c(6e-01, 2e-01, 1e-01, 1e-01)
+# int_frq <- c(5e-01, 2e-01, 2e-01, 1e-01)
 # evt_gen <- 210
 # int_gen <- 0
 # lst_gen <- 500
@@ -106,7 +109,7 @@ source("./Code/Code v1.0/Code v1.2/RFUN_COL.R")
 # rec_rat <- 5e-01
 # pop_siz <- c(rep(1e+04, length.out = 201), rep(5e+03, length.out = 200), rep(1e+04, length.out = 100))
 # ref_siz <- 1e+04
-# int_frq <- c(6e-01, 2e-01, 1e-01, 1e-01)
+# int_frq <- c(5e-01, 2e-01, 2e-01, 1e-01)
 # evt_gen <- 210
 # int_gen <- 0
 # lst_gen <- 500
@@ -158,7 +161,7 @@ source("./Code/Code v1.0/Code v1.2/RFUN_COL.R")
 # sel_cof <- matrix(c(0e+00, 5e-03, 1e-02, 5e-03), nrow = 2, ncol = 2)
 # rec_rat <- 5e-01
 # pop_siz <- c(rep(1e+04, length.out = 201), rep(5e+03, length.out = 200), rep(1e+04, length.out = 100))
-# int_frq <- c(6e-01, 2e-01, 1e-01, 1e-01)
+# int_frq <- c(5e-01, 2e-01, 2e-01, 1e-01)
 # evt_gen <- 210
 # smp_gen <- (0:10) * 50
 # smp_siz <- rep(100, 11)
@@ -226,7 +229,7 @@ source("./Code/Code v1.0/Code v1.2/RFUN_COL.R")
 # sel_cof <- matrix(c(0e+00, 5e-03, 1e-02, 5e-03), nrow = 2, ncol = 2)
 # rec_rat <- 5e-01
 # pop_siz <- c(rep(1e+04, length.out = 201), rep(5e+03, length.out = 200), rep(1e+04, length.out = 100))
-# int_frq <- c(6e-01, 2e-01, 1e-01, 1e-01)
+# int_frq <- c(5e-01, 2e-01, 2e-01, 1e-01)
 # evt_gen <- 210
 # smp_gen <- (0:10) * 50
 # smp_siz <- rep(100, 11)
@@ -299,10 +302,10 @@ model <- "WFM"
 sel_cof <- matrix(c(0e+00, 5e-03, 1e-02, 5e-03), nrow = 2, ncol = 2)
 rec_rat <- 5e-01
 pop_siz <- c(rep(1e+04, length.out = 201), rep(5e+03, length.out = 200), rep(1e+04, length.out = 100))
-int_frq <- c(6e-01, 2e-01, 1e-01, 1e-01)
+int_frq <- c(5e-01, 2e-01, 2e-01, 1e-01)
 evt_gen <- 210
-smp_gen <- (0:10) * 50
-smp_siz <- rep(100, 11)
+smp_gen <- (0:20) * 25
+smp_siz <- rep(100, 21)
 
 sim_HMM_WFM <- cmpsimulateHMM(model, sel_cof, rec_rat, pop_siz, int_frq, evt_gen, smp_gen, smp_siz)
 smp_gen <- sim_HMM_WFM$smp_gen
@@ -776,7 +779,7 @@ save(sel_cof, rec_rat, pop_siz, ref_siz, evt_gen, smp_gen, smp_siz, smp_cnt, ptn
 #
 # pdf(file = "./Output/Output v1.0/Test v1.2/TEST_COL_PMMH_Posterior_SelChange.pdf", width = 16, height = 16)
 # par(mar = c(5.5, 5, 5.5, 2.5), cex.main = 1.75, cex.sub = 1.5, cex.axis = 1.5, cex.lab = 1.5)
-# layout(matrix(c(1, 3, 2, 4), nrow = 2, ncol = 2))
+# layout(matrix(c(1, 1, 3, 3, 2, 2, 4, 5), nrow = 4, ncol = 2))
 # grd_num <- 1e+03
 # sel_cof_pdf <- kde2d(sel_cof_chn[1, 1, ], sel_cof_chn[1, 2, ], n = grd_num)
 # image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32),
@@ -787,15 +790,6 @@ save(sel_cof, rec_rat, pop_siz, ref_siz, evt_gen, smp_gen, smp_siz, smp_cnt, ptn
 # abline(v = sel_cof_est[1, 1], col = 'black', lty = 2, lwd = 2)
 # abline(h = sel_cof_est[1, 2], col = 'black', lty = 2, lwd = 2)
 #
-# hist(dif_sel_chn[1, ], breaks = seq(min(dif_sel_chn[1, ]), max(dif_sel_chn[1, ]), length.out = 50), freq = FALSE,
-#      xlab = "Change in the selection coefficient",
-#      main = "Posterior for the change in the sel coeff of the black")
-# lines(density(dif_sel_chn[1, ]), lwd = 2, col = 'black')
-# abline(v = 0, col = 'red', lty = 2, lwd = 2)
-# abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
-# abline(v = dif_sel_hpd[1, 1], col = 'blue', lty = 2, lwd = 2)
-# abline(v = dif_sel_hpd[1, 2], col = 'blue', lty = 2, lwd = 2)
-#
 # grd_num <- 1e+03
 # sel_cof_pdf <- kde2d(sel_cof_chn[2, 1, ], sel_cof_chn[2, 2, ], n = grd_num)
 # image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32),
@@ -805,6 +799,26 @@ save(sel_cof, rec_rat, pop_siz, ref_siz, evt_gen, smp_gen, smp_siz, smp_cnt, ptn
 # abline(h = sel_cof[2, 2], col = 'red', lty = 2, lwd = 2)
 # abline(v = sel_cof_est[2, 1], col = 'black', lty = 2, lwd = 2)
 # abline(h = sel_cof_est[2, 2], col = 'black', lty = 2, lwd = 2)
+#
+# grd_num <- 1e+03
+# sel_cof_pdf <- kde2d(dif_sel_chn[1, ], dif_sel_chn[2, ], n = grd_num)
+# image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32),
+#       xlab = "Change in the selection coefficient of the black", ylab = "Change in the selection coefficient of the chestnut",
+#       main = "Joint posterior for the change in the sel coeff before and after the event")
+# abline(v = 0, col = 'red', lty = 2, lwd = 2)
+# abline(h = 0, col = 'red', lty = 2, lwd = 2)
+# abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
+# abline(h = dif_sel_est[2], col = 'black', lty = 2, lwd = 2)
+# HPDregionplot(as.mcmc(t(dif_sel_chn)), vars = 1:2, n = grd_num, prob = 0.95, col = "blue", lwd = 2, add = TRUE)
+#
+# hist(dif_sel_chn[1, ], breaks = seq(min(dif_sel_chn[1, ]), max(dif_sel_chn[1, ]), length.out = 50), freq = FALSE,
+#      xlab = "Change in the selection coefficient",
+#      main = "Posterior for the change in the sel coeff of the black")
+# lines(density(dif_sel_chn[1, ]), lwd = 2, col = 'black')
+# abline(v = 0, col = 'red', lty = 2, lwd = 2)
+# abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
+# abline(v = dif_sel_hpd[1, 1], col = 'blue', lty = 2, lwd = 2)
+# abline(v = dif_sel_hpd[1, 2], col = 'blue', lty = 2, lwd = 2)
 #
 # hist(dif_sel_chn[2, ], breaks = seq(min(dif_sel_chn[2, ]), max(dif_sel_chn[2, ]), length.out = 50), freq = FALSE,
 #      xlab = "Change in the selection coefficient",
@@ -995,7 +1009,7 @@ save(sel_cof, rec_rat, pop_siz, ref_siz, evt_gen, smp_gen, smp_siz, smp_cnt, ptn
 #
 # pdf(file = "./Output/Output v1.0/Test v1.2/TEST_COL_BayesProc_Posterior_SelChange.pdf", width = 16, height = 16)
 # par(mar = c(5.5, 5, 5.5, 2.5), cex.main = 1.75, cex.sub = 1.5, cex.axis = 1.5, cex.lab = 1.5)
-# layout(matrix(c(1, 3, 2, 4), nrow = 2, ncol = 2))
+# layout(matrix(c(1, 1, 3, 3, 2, 2, 4, 5), nrow = 4, ncol = 2))
 # grd_num <- 1e+03
 # sel_cof_pdf <- kde2d(sel_cof_chn[1, 1, ], sel_cof_chn[1, 2, ], n = grd_num)
 # image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32),
@@ -1006,15 +1020,6 @@ save(sel_cof, rec_rat, pop_siz, ref_siz, evt_gen, smp_gen, smp_siz, smp_cnt, ptn
 # abline(v = sel_cof_est[1, 1], col = 'black', lty = 2, lwd = 2)
 # abline(h = sel_cof_est[1, 2], col = 'black', lty = 2, lwd = 2)
 #
-# hist(dif_sel_chn[1, ], breaks = seq(min(dif_sel_chn[1, ]), max(dif_sel_chn[1, ]), length.out = 50), freq = FALSE,
-#      xlab = "Change in the selection coefficient",
-#      main = "Posterior for the change in the sel coeff of the black")
-# lines(density(dif_sel_chn[1, ]), lwd = 2, col = 'black')
-# abline(v = 0, col = 'red', lty = 2, lwd = 2)
-# abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
-# abline(v = dif_sel_hpd[1, 1], col = 'blue', lty = 2, lwd = 2)
-# abline(v = dif_sel_hpd[1, 2], col = 'blue', lty = 2, lwd = 2)
-#
 # grd_num <- 1e+03
 # sel_cof_pdf <- kde2d(sel_cof_chn[2, 1, ], sel_cof_chn[2, 2, ], n = grd_num)
 # image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32),
@@ -1024,6 +1029,26 @@ save(sel_cof, rec_rat, pop_siz, ref_siz, evt_gen, smp_gen, smp_siz, smp_cnt, ptn
 # abline(h = sel_cof[2, 2], col = 'red', lty = 2, lwd = 2)
 # abline(v = sel_cof_est[2, 1], col = 'black', lty = 2, lwd = 2)
 # abline(h = sel_cof_est[2, 2], col = 'black', lty = 2, lwd = 2)
+#
+# grd_num <- 1e+03
+# sel_cof_pdf <- kde2d(dif_sel_chn[1, ], dif_sel_chn[2, ], n = grd_num)
+# image(sel_cof_pdf, col = colorRampPalette(rev(brewer.pal(11, 'Spectral')))(32),
+#       xlab = "Change in the selection coefficient of the black", ylab = "Change in the selection coefficient of the chestnut",
+#       main = "Joint posterior for the change in the sel coeff before and after the event")
+# abline(v = 0, col = 'red', lty = 2, lwd = 2)
+# abline(h = 0, col = 'red', lty = 2, lwd = 2)
+# abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
+# abline(h = dif_sel_est[2], col = 'black', lty = 2, lwd = 2)
+# HPDregionplot(as.mcmc(t(dif_sel_chn)), vars = 1:2, n = grd_num, prob = 0.95, col = "blue", lwd = 2, add = TRUE)
+#
+# hist(dif_sel_chn[1, ], breaks = seq(min(dif_sel_chn[1, ]), max(dif_sel_chn[1, ]), length.out = 50), freq = FALSE,
+#      xlab = "Change in the selection coefficient",
+#      main = "Posterior for the change in the sel coeff of the black")
+# lines(density(dif_sel_chn[1, ]), lwd = 2, col = 'black')
+# abline(v = 0, col = 'red', lty = 2, lwd = 2)
+# abline(v = dif_sel_est[1], col = 'black', lty = 2, lwd = 2)
+# abline(v = dif_sel_hpd[1, 1], col = 'blue', lty = 2, lwd = 2)
+# abline(v = dif_sel_hpd[1, 2], col = 'blue', lty = 2, lwd = 2)
 #
 # hist(dif_sel_chn[2, ], breaks = seq(min(dif_sel_chn[2, ]), max(dif_sel_chn[2, ]), length.out = 50), freq = FALSE,
 #      xlab = "Change in the selection coefficient",
