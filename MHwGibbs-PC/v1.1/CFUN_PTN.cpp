58c9301d165ad7c0d2aab7f57ac9dc77c8fc64d8
zhangyi-he <jefferyhe@outlook.com>
2021-05-30 10:07:38 +0100

    Update CFUN_PTN.cpp

--- MHwGibbs-PC/v1.1/CFUN_PTN.cpp
+++ MHwGibbs-PC/v1.1/CFUN_PTN.cpp
@@ -1,8 +1,9 @@
-// A Bayesian approach for estimating selection coefficients and testing their changes from ancient DNA data
-// Xiaoyang Dai, Mark Beaumont, Feng Yu, Ludovic Orlando, Zhangyi He
+// Estimating selection coefficients and testing their changes from ancient DNA data
+// Xiaoyang Dai, Mark Beaumont, Feng Yu, Zhangyi He
 
 // version 1.1
-// Horse coat patterns (KIT13 & KIT116) under constant natural selection and non-constant demographic histories (N/A is not allowed)
+// Two-gene phenotypes under constant natural selection and non-constant demographic histories
+// Horse white coat patterns (KIT13 & KIT116)
 
 // C functions
 
@@ -272,7 +273,7 @@ double calculateEmissionProb_arma(const arma::icolvec& smp_cnt, const int& smp_s
   return prob;
 }
 
-// Initialise the particles in the particle filter (uniform generation from the flat Dirichlet distribution)
+// Initialise the particles in the particle filter
 // [[Rcpp::export]]
 arma::dmat initialiseParticle_arma(const arma::uword& pcl_num){
   // ensure RNG gets set/reset
@@ -634,7 +635,9 @@ arma::dmat runAdaptPMMH_arma(const arma::dcolvec& sel_cof, const double& rec_rat
   arma::drowvec log_lik = arma::zeros<arma::drowvec>(2);
 
   arma::dcolvec U = arma::zeros<arma::dcolvec>(3);
-  arma::dmat S = {{5e-03, 0e-00, 0e-00}, {0e-00, 5e-03, 0e-00}, {0e-00, 0e-00, 5e-03}};
+  arma::dmat S = {{5e-03, 0e-00, 0e-00}, 
+                  {0e-00, 5e-03, 0e-00}, 
+                  {0e-00, 0e-00, 5e-03}};
   arma::dmat M = arma::zeros<arma::dmat>(3, 3);
   arma::dmat I = arma::eye<arma::dmat>(3, 3);
 
