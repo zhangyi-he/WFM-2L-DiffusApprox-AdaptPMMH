29854c4a27a5fa326086a03fff81c53663232a56
zhangyi-he <jefferyhe@outlook.com>
2021-06-03 11:30:25 +0100

    Update RFUN_COL.R

--- 2L/v1.0/RFUN_COL.R
+++ 2L/v1.0/RFUN_COL.R
@@ -2,7 +2,9 @@
 #' @author Xiaoyang Dai, Mark Beaumont, Feng Yu, Zhangyi He
 
 #' version 1.0
-#' Two-gene phenotypes under constant natural selection and constant demographic histories
+#' Phenotypes controlled by two genes (genetic linkage and epistatic interaction)
+#' Constant natural selection and constant demographic histories
+
 #' Horse base coat colours (ASIP & MC1R)
 
 #' R functions
@@ -34,8 +36,8 @@ sourceCpp("./Code/Code v1.0/Code 2L/Code v1.0/CFUN_COL.cpp")
 
 #' Simulate the haplotype frequency trajectories according to the two-locus Wright-Fisher model with selection
 #' Parameter setting
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the generation that the simulated haplotype frequency trajectories started
@@ -59,8 +61,8 @@ cmpsimulateWFM <- cmpfun(simulateWFM)
 
 #' Simulate the haplotype frequency trajectories according to the two-locus Wright-Fisher diffusion with selection using the Euler-Maruyama method
 #' Parameter setting
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param int_frq the initial haplotype frequencies of the population
 #' @param int_gen the generation that the simulated haplotype frequency trajectories started
@@ -70,13 +72,13 @@ cmpsimulateWFM <- cmpfun(simulateWFM)
 
 #' Standard version
 simulateWFD <- function(sel_cof, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num, dat_aug = TRUE) {
-  frq_pth <- simulateWFD_arma(sel_cof, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num)
-  frq_pth <- as.matrix(frq_pth)
+  hap_frq_pth <- simulateWFD_arma(sel_cof, rec_rat, pop_siz, int_frq, int_gen, lst_gen, ptn_num)
+  hap_frq_pth <- as.matrix(hap_frq_pth)
 
   if (dat_aug == FALSE) {
-    return(frq_pth[, (0:(lst_gen - int_gen)) * ptn_num + 1])
+    return(hap_frq_pth[, (0:(lst_gen - int_gen)) * ptn_num + 1])
   } else {
-    return(frq_pth)
+    return(hap_frq_pth)
   }
 }
 #' Compiled version
@@ -87,8 +89,8 @@ cmpsimulateWFD <- cmpfun(simulateWFD)
 #' Simulate the hidden Markov model
 #' Parameter setting
 #' @param model = "WFM"/"WFD" (return the observations from the underlying population evolving according to the WFM or the WFD)
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param int_con the initial haplotype frequencies of the population / the initial mutant allele frequencies and the linkage disequilibrium of the population
 #' @param smp_gen the sampling time points measured in one generation
@@ -167,8 +169,8 @@ cmpsimulateHMM <- cmpfun(simulateHMM)
 
 #' Run the bootstrap particle filter (BPF) with the two-locus Wright-Fisher diffusion with selection
 #' Parameter setting
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param smp_gen the sampling time points measured in one generation
 #' @param smp_siz the count of the horses drawn from the population at all sampling time points
@@ -208,8 +210,8 @@ cmprunBPF <- cmpfun(runBPF)
 
 #' Calculate the optimal particle number in the particle marginal Metropolis-Hastings (PMMH)
 #' Parameter settings
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param smp_gen the sampling time points measured in one generation
 #' @param smp_siz the count of the horses drawn from the population at all sampling time points
@@ -235,8 +237,8 @@ cmpcalculateOptimalParticleNum <- cmpfun(calculateOptimalParticleNum)
 
 #' Run the particle marginal Metropolis-Hastings (PMMH)
 #' Parameter settings
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param smp_gen the sampling time points measured in one generation
 #' @param smp_siz the count of the horses drawn from the population at all sampling time points
@@ -262,8 +264,8 @@ cmprunPMMH <- cmpfun(runPMMH)
 
 #' Run the adaptive particle marginal Metropolis-Hastings (AdaptPMMH)
 #' Parameter settings
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param smp_gen the sampling time points measured in one generation
 #' @param smp_siz the count of the horses drawn from the population at all sampling time points
@@ -291,8 +293,8 @@ cmprunAdaptPMMH <- cmpfun(runAdaptPMMH)
 
 #' Run the Bayesian procedure for the inference of natural selection
 #' Parameter settings
-#' @param sel_cof the selection coefficients of the black and chestnut phenotypes
-#' @param rec_rat the recombination rate between the ASIP and MC1R loci
+#' @param sel_cof the selection coefficients of the black and chestnut against bay
+#' @param rec_rat the (artificial) recombination rate between the ASIP and MC1R loci (r = 0.5)
 #' @param pop_siz the size of the horse population (constant)
 #' @param smp_gen the sampling time points measured in one generation
 #' @param smp_siz the count of the horses drawn from the population at all sampling time points
