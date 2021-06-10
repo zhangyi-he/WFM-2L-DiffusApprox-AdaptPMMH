96fd50b7c9f6f60bbd2e96c22d90c70be3b0a9de
zhangyi-he <jefferyhe@outlook.com>
2021-06-10 23:38:33 +0100

    Update REAL_COL.R

--- 2L/v1.2/REAL_COL.R
+++ 2L/v1.2/REAL_COL.R
@@ -382,7 +382,7 @@ dev.off()
 #' Raw data of Wutke et al. (2016) from 9320 BC (Holocene 9700 BC)
 load("./Data/REAL_RAW_COL.rda")
 
-set.seed(3)
+set.seed(11) # 3
 
 sel_cof <- matrix(c(0e+00, 0e+00, 0e+00, 0e+00), nrow = 2, ncol = 2)
 rec_rat <- 5e-01
