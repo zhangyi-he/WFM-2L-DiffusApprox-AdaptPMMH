4dee668df9356823ac699c696f8bb773971dd0b1
zhangyi-he <jefferyhe@outlook.com>
2022-07-20 00:37:22 +0100

    Update RFUN_PTN.R

--- Code v1.2/RFUN_PTN.R
+++ Code v1.2/RFUN_PTN.R
@@ -5,6 +5,8 @@
 #' Phenotypes controlled by two genes with genetic linkage
 #' Non-constant natural selection and non-constant demographic histories
 
+#' Use the flat Dirichlet prior for the starting haplotype frequencies of the underlying population
+
 #' Input: genotype likelihoods
 #' Output: posteriors for the selection coefficient and the genotype frequency trajectories of the population
 
